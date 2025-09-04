43b258085219e88056563fd6b778ea6ac7a32c80
Giovanna Ianiero <Ianierogiovanna@gmail.com>
2025-09-04 12:30:46 -0300

    modifique errores de ejercicio

--- primerelemento.js
+++ primerelemento.js
@@ -1,11 +1,11 @@
 /*Eliminar el primer elemento de un array: 
 Crea una función que reciba un array y elimine su primer elemento usando .shift().*/
 
-function primerElemento(Array){
-    Array.shift(primerElemento);
-    console.log(Array);
+function primerElemento(array) {
+    array.shift();
+    console.log(array);
 
-
-    let vehiculos = ['auto', 'moto', 'camión','bici'];
-    primerElemento(vehiculos);
 }
+
+let vehiculos = ['auto', 'moto', 'camión', 'bici'];
+primerElemento(vehiculos);
