66c2d29045c6e60e01323efbd86325c460f3d6ac
Giovanna Ianiero <Ianierogiovanna@gmail.com>
2025-09-04 13:08:47 -0300

    creo la funcion con sus parámetros

--- eliminarelemt.js
+++ eliminarelemt.js
@@ -1,2 +1,8 @@
 /*Eliminar un elemento de un array según su posición: 
 Crea una función que reciba un array y una posición, y elimine el elemento con .splice().*/
+
+function eliminarElemento(array, posicion) {
+    array.splice(posicion, 1);
+    console.log(array);
+
+}
