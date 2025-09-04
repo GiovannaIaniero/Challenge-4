c49f56b8c37146363e82d128432accd6d8c8ef6a
Giovanna Ianiero <Ianierogiovanna@gmail.com>
2025-09-04 12:59:30 -0300

    terminé ejercicio

--- letrasdeuntexto.js
+++ letrasdeuntexto.js
@@ -1,3 +1,16 @@
 /*Contar cuántas veces aparece una letra en un texto: 
 Crea una función que reciba un texto y una letra, y cuente cuántas veces aparece usando un for.*/
 
+function contarLetras(texto, Letra) {
+    let contador = 0;
+
+    for (let i = 0; i < texto.length; i++) {
+        if (texto[i] === Letra) {
+            contador++;
+        }
+    }
+    console.log(`La letra "${Letra}" aparece ${contador} veces en el texto.`);
+}
+let textoUsuario = prompt("Ingrese un texto:");
+let LetraUsuario = prompt("Ingrese una letra para buscar en el texto:");
+contarLetras(textoUsuario, LetraUsuario)
