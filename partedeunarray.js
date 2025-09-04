a2a02d034ff03dd49f5cf9aa3bb74acf86d66776
Giovanna Ianiero <Ianierogiovanna@gmail.com>
2025-09-04 18:41:46 -0300

    desarrollando ejercicio

---
+++ partedeunarray.js
@@ -0,0 +1,12 @@
+/*Extraer una parte de un array: 
+Crea una función que reciba un array y devuelva una parte usando .slice().*/
+
+
+function partedearray( array, inicio, fin){
+    
+}
+
+
+let vocales = ['a', 'e', 'i','o', 'u'];
+let parteVocales = vocales.slice(0,3);
+document.writeln(parteVocales);
