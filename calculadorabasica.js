e76b1619b85caf5cbadce4cb8bfb6f8d875b66d4
Giovanna Ianiero <Ianierogiovanna@gmail.com>
2025-09-04 18:23:49 -0300

    desarrollando ejercicio

---
+++ calculadorabasica.js
@@ -0,0 +1,15 @@
+/*Calculadora básica con switch: 
+Crea una función que reciba dos números y un operador (+, -, *, /). 
+Devuelva el resultado de la operación. Si el operador no es válido, mostrar error.*/
+
+fuction calculadoraBasica(num1, num2, operador){
+    let resultado;
+
+    switch(operador){
+
+    }
+   
+}
+let numero1 = parseFloat(prompt("Ingrese el primer número:"));
+let numero2 = parseFloat(prompt("Ingrese el segundo número:"));
+let operador = prompt("Ingrese el operador (+, -, *, /):");
